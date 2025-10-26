c8402c08b78ace9a494ab49392bca21b79a857bc
simuneu <worksimu@gmail.com>
2025-10-26 15:42:02 +0900

    feat:로그인, 회원가입 페이지 연결

--- my-tailwind/src/main.tsx
+++ my-tailwind/src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './App.css'
+import AuthPage from './page/AuthPage.tsx'
+import axios from 'axios';
+axios.defaults.baseURL = "http://localhost:8080";
+axios.defaults.withCredentials = true;
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <AuthPage />
   </React.StrictMode>,
 )
